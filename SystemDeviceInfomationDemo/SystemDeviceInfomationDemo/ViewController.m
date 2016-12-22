026f142594920c94f350c14367f9c43d7d84f306
thomas <dui1cuo@126.com>
2016-12-22 18:43:43 +0800

    [Mod]minor fix

--- SystemDeviceInfomationDemo/SystemDeviceInfomationDemo/ViewController.m
+++ SystemDeviceInfomationDemo/SystemDeviceInfomationDemo/ViewController.m
@@ -8,21 +8,73 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
 
+#pragma mark - Lifecycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Private
+
+//获取电池电量
+-(CGFloat)getBatteryQuantity
+{
+    return [[UIDevice currentDevice] batteryLevel];
+}
+
+#pragma mark - Handlers
+
+- (IBAction)batteryQuantityButtonWasPressed:(UIButton *)sender {
+    CGFloat battery = [self getBatteryQuantity];
+    NSLog(@"获取电池电量:::::%@",@(battery));
+    [sender setTitle:[NSString stringWithFormat:@"%@",@(battery)] forState:UIControlStateNormal];
+}
+
+- (IBAction)batteryStatusButtonWasPressed:(UIButton *)sender {
+    NSLog(@"获取电池状态");
+}
+
+- (IBAction)totalMemorySizeButtonWasPressed:(UIButton *)sender {
+    NSLog(@"获取内存大小");
+}
+
+- (IBAction)availableMemorySizeButtonWasPressed:(UIButton *)sender {
+    NSLog(@"获取可用内存");
+}
+
+- (IBAction)usedMemoryButtonWasPressed:(UIButton *)sender {
+    NSLog(@"获取已使用内存");
+}
+
+- (IBAction)totalDiskSizeButtonWasPressed:(UIButton *)sender {
+    NSLog(@"获取总磁盘容量");
+}
+
+- (IBAction)availableDiskSizeButtonWasPressed:(UIButton *)sender {
+    NSLog(@"获取可用磁盘容量");
+}
+
+- (IBAction)currentDeviceTypeButtonWasPressed:(UIButton *)sender {
+    NSLog(@"获取设备型号");
+}
+
+- (IBAction)deviceIdAddressButtonWasPressed:(UIButton *)sender {
+    NSLog(@"获取IP地址");
+}
+
+- (IBAction)phoneWifiNameButtonWasPressed:(UIButton *)sender {
+    NSLog(@"获取手机WifI名称");
 }
 
 
